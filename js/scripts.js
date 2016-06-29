d7d910b0edf8e3af047ec1b1bcec3754ba675c13
Andrew Niekamp and Russell Davies <andrew+russell@>
2016-06-28 17:01:22 -0700

    Add funcitonality to enact account initialization and CSS styling

--- js/scripts.js
+++ js/scripts.js
@@ -10,21 +10,36 @@ BankAccount.prototype.withdraw = function(amount) {
   this.balance -= amount;
 }
 
+var clearForm = function () {
+  $("#customer-name").val('');
+  $("#initialDeposit").val('');
+  $("#withdrawEntry").val('');
+  $("#depositEntry").val('');
+};
+
 var bank = null;
 $(document).ready(function(){
+
   $("#createAccount").click(function(){
     var initDeposit = parseInt($("#initialDeposit").val());
     var customerName = $("#customer-name").val();
     bank = new BankAccount (customerName, initDeposit);
+    $("#output").html("<h3>Thank you for registering, " + customerName + "!</h3> Initial balance deposited is: $" + bank.balance + ".</h4>")
+    clearForm();
+
     $("#withdrawButton").click(function(){
-      bank.withdraw(parseInt($("#withdrawEntry").val()));
-      console.log(bank);
+      var withdrawal = parseInt($("#withdrawEntry").val());
+      bank.withdraw(withdrawal);
+      $("#output").html("<h4>You have withdrawn $" + withdrawal + " from your account.</h4><h4>Current balance: $" + bank.balance + ".</h4");
+      clearForm();
     });
 
     $("#depositButton").click(function(){
-      bank.deposit(parseInt($("#depositEntry").val()));
-      console.log(bank);
+      var deposit = parseInt($("#depositEntry").val());
+      bank.deposit(deposit);
+      $("#output").html("<h4>You have deposited $" + deposit + " into your account.</h4><h4>Current balance: $" + bank.balance + ".</h4");
+      clearForm();
+
     });
-    console.log(bank);
   });
 });
